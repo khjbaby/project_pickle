d922e5ed524caeff673c7b1ceec0267a70eaee3a
khjbaby <hayleyso1004@gmail.com>
2020-05-14 16:24:32 +0900

    dictionary version

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import JsonResponse
 def home(request):
-    return HttpResponse("Hello, Djangooooooo!")
+    name1=request.GET['name']
+    age2=request.GET['AGE']
+    requestDict=request.GET
+    result=int(age2)+7
+    requestDict={'result_ohhoo':result}
+    return JsonResponse(requestDict)
